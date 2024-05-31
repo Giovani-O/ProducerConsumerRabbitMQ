c2a068f59573765e623eb892c03ae54dcea305b2
Giovani <giovani98@proton.me>
2024-05-31 13:46:24 -0300

    feat: Create sender

--- Producer/Sender.cs
+++ Producer/Sender.cs
@@ -1,9 +1,32 @@
-﻿namespace Producer;
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Producer;
 
 public class Sender
 {
     public static void Main(string[] args)
     {
-        
+        // Cria factory de conexão
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        // Abre uma conexão
+        using(var connection = factory.CreateConnection())
+        // Abre um canal para a criação de filas
+        using(var channel = connection.CreateModel())
+        {
+            // Declara uma fila
+            channel.QueueDeclare("BasicTest", false, false, false, null);
+
+            // Escreve e criptografa uma mensagem
+            string message = "Primeiros passos com .NET Core RabbitMQ";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            // Publica a mensagem na fila
+            channel.BasicPublish("", "BasicTest", null, body);
+            Console.WriteLine("Mensagem enviada: {0}...", message);
+        }
+        Console.WriteLine("Press enter to exit...");
+        Console.ReadLine();
     }
 }
--- Producer/obj/Debug/net8.0/Producer.AssemblyInfo.cs
+++ Producer/obj/Debug/net8.0/Producer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Producer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf3a22e2cde8c694bc4b83d2e062a2973d6d05d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Producer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Producer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
